0b35ce60cbde7a55e1e4ff2462cb93185a79b4a6
Rahul <rahulprajapati.pranton@gmail.com>
2024-07-10 13:23:30 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.json({msg: "Testing"});
+})
+
+app.post("/custom", (req, res) => {
+    console.log("someone sent a request over here ", req.body);
+    res.status(200).send("done")
+})
+
+app.listen(4000, () => {
+    console.log('server is running on port 4000')
+})
